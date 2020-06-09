73965a588d86da20f128be3600122593bb10ef47
Francesco <med.dama@gmail.com>
2020-06-09 17:49:15 +0100

    first commit

---
+++ src/test/java/com/github/Ciiccillo73/Files/fileManagment/FileManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Ciiccillo73.Files.fileManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

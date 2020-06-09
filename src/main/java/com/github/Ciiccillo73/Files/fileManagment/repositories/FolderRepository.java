194bdac44f30079aca6022a5ff01948fb3dd0ec0
Francesco <med.dama@gmail.com>
2020-06-09 18:17:37 +0100

    Create Folder interface Repository

--- src/main/java/com/github/Ciiccillo73/Files/fileManagment/repositories/FolderRepository.java
+++ src/main/java/com/github/Ciiccillo73/Files/fileManagment/repositories/FolderRepository.java
@@ -1,10 +1,10 @@
 package com.github.Ciiccillo73.Files.fileManagment.repositories;
 
-import com.github.Ciiccillo73.Files.fileManagment.models.User;
+
+import com.github.Ciiccillo73.Files.fileManagment.models.Folder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface FolderRepository extends JpaRepository<Folder, Long> {
 }

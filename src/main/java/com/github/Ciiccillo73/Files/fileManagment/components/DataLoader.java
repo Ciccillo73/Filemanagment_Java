115018edea46520343e088e75219eddf893e07b5
Francesco <med.dama@gmail.com>
2020-06-09 18:33:25 +0100

    Fill up DataLoader

--- src/main/java/com/github/Ciiccillo73/Files/fileManagment/components/DataLoader.java
+++ src/main/java/com/github/Ciiccillo73/Files/fileManagment/components/DataLoader.java
@@ -1,5 +1,8 @@
 package com.github.Ciiccillo73.Files.fileManagment.components;
 
+import com.github.Ciiccillo73.Files.fileManagment.models.File;
+import com.github.Ciiccillo73.Files.fileManagment.models.Folder;
+import com.github.Ciiccillo73.Files.fileManagment.models.User;
 import com.github.Ciiccillo73.Files.fileManagment.repositories.FileRepository;
 import com.github.Ciiccillo73.Files.fileManagment.repositories.FolderRepository;
 import com.github.Ciiccillo73.Files.fileManagment.repositories.UserRepository;
@@ -21,12 +24,46 @@ public class DataLoader  implements ApplicationRunner {
     @Autowired
     FileRepository fileRepository;
 
-    public DataLoader(){}
-}
+    public DataLoader() {
+    }
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
+        User francesco = new User("Francesco");
+        userRepository.save(francesco);
+
+        User antonio = new User("Antonio");
+        userRepository.save(antonio);
+
+        User john = new User("John");
+        userRepository.save(john);
+
+        Folder folder1 = new Folder("papers", francesco);
+        folderRepository.save(folder1);
+
+        Folder folder2 = new Folder("photos", francesco);
+        folderRepository.save(folder2);
+
+        Folder folder3 = new Folder("places", francesco);
+        folderRepository.save(folder3);
+
+        File file1 = new File("pic1", ".jpg",3456, folder1);
+        fileRepository.save(file1);
+        File file2 = new File("install", ".exe",5465, folder2);
+        fileRepository.save(file2);
+
+        File file3 = new File("document", ".doc",52334, folder3);
+        fileRepository.save(file3);
+
+
+
+
+
     }
+
+
+}
 
 

49334ac9cdc9b8d0c493de094c5dc15b694d7e2f
Francesco <med.dama@gmail.com>
2020-06-09 18:47:15 +0100

    Create route

--- src/main/java/com/github/Ciiccillo73/Files/fileManagment/controller/UserController.java
+++ src/main/java/com/github/Ciiccillo73/Files/fileManagment/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,6 +30,15 @@ public class UserController {
     public ResponseEntity getUSerById(@PathVariable Long id){
         return new ResponseEntity(userRepository.findById(id), HttpStatus.OK);
     }
+
+    //create route, create a new instance of user
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
 
 
 }
